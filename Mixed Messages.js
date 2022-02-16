bfdbcf8777ab4dae2bc7cd4479d7e72c248f7909
Shawn Code <jeremydeshawn@rocketmail.com>
2022-02-15 19:54:55 -0600

    The arrays for my mixed messages

---
+++ Mixed Messages.js
@@ -0,0 +1,3 @@
+const start = ['I didn\'t know how well ', 'You won\'t believe how well ', 'I like money as much as '];
+const noun = ['Jay','Cee','May','Shawn','Jayshawn','Kaileyy','Christian','Carter'];
+const adverb =['runs.','swims!','leaps.','plays!','fights.','laughs!','skateboards.'];
