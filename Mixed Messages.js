94360b2d762c2680c7549a7f3df142ddf1639485
Shawn Code <jeremydeshawn@rocketmail.com>
2022-02-16 20:11:04 -0600

    the random string generators

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -1,3 +1,10 @@
-const start = ['I didn\'t know how well ', 'You won\'t believe how well ', 'I like money as much as '];
+//arrays with strings for random messages
+const start = ['I didn\'t know how often ', 'You won\'t believe how much ', 'I like money as much as ', 'I love it when '];
 const noun = ['Jay','Cee','May','Shawn','Jayshawn','Kaileyy','Christian','Carter'];
-const adverb =['runs.','swims!','leaps.','plays!','fights.','laughs!','skateboards.'];
+const adverb =[' runs.',' swims!',' leaps.',' plays!',' fights.',' laughs!',' skateboards.'];
+//random string generators
+const randomStart = start[Math.floor(Math.random() * start.length)];
+const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+const randomAdverb = adverb[Math.floor(Math.random() * adverb.length)];
+
+
